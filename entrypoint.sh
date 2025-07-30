9f9b3046f1416222f835ad429812efd78e6bea74
Kaiky Cabral do Patrocinio <kaiky.cabral@smfniteroi.rj.gov.br>
2025-07-30 16:54:18 -0300

    Adicionando Log detalhada no entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+echo "Entrypoint started" >> /home/site/wwwroot/entrypoint.log 2>&1
+
 if [ ! -f /home/site/wwwroot/index.php ]; then
+    echo "Copiando arquivos padrão do WordPress..." >> /home/site/wwwroot/entrypoint.log 2>&1
     cp -a /var/www/html/. /home/site/wwwroot/
     chown -R www-data:www-data /home/site/wwwroot
 fi
 
+echo "Iniciando Apache..." >> /home/site/wwwroot/entrypoint.log 2>&1
 exec docker-entrypoint.sh apache2-foreground
